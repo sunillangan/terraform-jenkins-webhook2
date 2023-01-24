732c612e4e2b0e779ed690cf28eacc68939b8ea1
Sunil <sunil@gmail.com>
2023-01-25 02:12:40 +0800

    initial commit

---
+++ sg.tf
@@ -0,0 +1,37 @@
+resource "aws_security_group" "allow_tls" {
+  name = "shah-security_group2"
+  description = "Allow TLS inbound traffic"
+
+  ingress {
+    from_port = 443
+    to_port = 443
+    protocol = "tcp"
+    cidr_blocks = var.vpn_ip
+  }
+
+  ingress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = var.vpn_ip
+  }
+
+  ingress {
+    from_port = 8080
+    to_port = 8080
+    protocol = "tcp"
+    cidr_blocks = var.vpn_ip
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "allow_tls"
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,3 @@
+variable "vpn_ip" {
+  default = ["10.196.30.40/32"]
+}
